ec242aff6e813e7277211df4a2b7b5e13514b740
Aleksandra Jensen <alekmisjen@gmail.com>
2024-03-01 12:41:11 +0100

    js file added with separate js file for the filters. worked on responsivness and small details

---
+++ js/filters.js
@@ -0,0 +1,78 @@
+document
+  .querySelector("#filter-coding")
+  .addEventListener("change", filterCoding);
+document
+  .querySelector("#filter-design")
+  .addEventListener("change", filterDesign);
+document
+  .querySelector("#filter-marketing")
+  .addEventListener("change", filterMarketing);
+function filterCoding() {
+  hideAllCards();
+
+  if (document.querySelector("#filter-coding").checked) {
+    var codingCards = document.querySelectorAll(".coding");
+    codingCards.forEach((codingCard) => {
+      codingCard.style.display = "inline-block";
+    });
+    document.querySelector("#filter-design").checked = false;
+    document.querySelector("#filter-marketing").checked = false;
+  } else {
+    showAllCards();
+  }
+}
+
+function filterDesign() {
+  hideAllCards();
+
+  if (document.querySelector("#filter-design").checked) {
+    var designCards = document.querySelectorAll(".design");
+    designCards.forEach((designCard) => {
+      designCard.style.display = "inline-block";
+    });
+
+    document.querySelector("#filter-coding").checked = false;
+    document.querySelector("#filter-marketing").checked = false;
+  } else {
+    showAllCards();
+  }
+}
+
+function filterMarketing() {
+  hideAllCards();
+
+  if (document.querySelector("#filter-marketing").checked) {
+    var marketingCards = document.querySelectorAll(".marketing");
+    marketingCards.forEach((marketingCard) => {
+      marketingCard.style.display = "inline-block";
+    });
+
+    document.querySelector("#filter-design").checked = false;
+    document.querySelector("#filter-coding").checked = false;
+  } else {
+    showAllCards();
+  }
+}
+
+function hideAllCards() {
+  var allCards = document.querySelectorAll(".custom-card");
+
+  allCards.forEach((card) => {
+    card.style.display = "none";
+  });
+}
+
+function showAllCards() {
+  var allCards = document.querySelectorAll(".custom-card");
+
+  allCards.forEach((card) => {
+    card.style.display = "inline-block";
+  });
+}
+const filterList = document.querySelectorAll(".filter");
+filterList.forEach((filter) => {
+  filter.addEventListener("click", () => {
+    document.querySelector(".active")?.classList.remove("active");
+    filter.classList.add("active");
+  });
+});
