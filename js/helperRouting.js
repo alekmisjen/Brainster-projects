b463e3e717e336267b0553f62d89ab6a65e33e7a
Aleksandra Jensen <alekmisjen@gmail.com>
2024-06-26 00:34:19 +0200

    handleRouting added

---
+++ js/helperRouting.js
@@ -0,0 +1,39 @@
+// export function hideAllSections() {
+//   document
+//     .querySelectorAll("section")
+//     .forEach((sectionElement = sectionElement.style.display = "none"));
+// }
+// //pages
+// const homePage = document.querySelector("#home");
+// const contentPage = document.querySelector("#content");
+// const discussionPage = document.querySelector("#discussion");
+// const contactPage = document.querySelector("#contact");
+// const profilePage = document.querySelector("#profile");
+// export function handleRoute() {
+//   const currentHashRoute = location.hash.slice(1);
+
+//   switch (currentHashRoute) {
+//     case "": {
+//       homePage.style.display = "block";
+
+//       break;
+//     }
+//     case "home": {
+//       homePage.style.display = "block";
+
+//       break;
+//     }
+//     case "content": {
+//       contentPage.style.display = "block";
+
+//       break;
+//     }
+//     case "discussion": {
+//       discussionPage.style.display = "block";
+
+//       break;
+//     }
+//     default:
+//       location.hash = "";
+//   }
+// }
